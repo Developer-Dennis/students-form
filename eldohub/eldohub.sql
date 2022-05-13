18b264f6b7fdd9c2df7807212b80b2bda18d1693
Developer-Dennis <denniskipkogei11@gmail.com>
2022-05-13 14:15:09 +0300

    student managment system form

---
+++ eldohub/eldohub.sql
@@ -0,0 +1,29 @@
+CREATE TABLE student(
+    id int,
+    Fullname char(9),
+    age int ,
+    
+    DOB date 
+);
+
+
+CREATE TABLE kids(
+    id int,
+    Fullname char(9),
+    age int,
+    DOB date
+);
+
+INSERT INTO student VALUES (1,'Sarah',50,2/2/1997);
+INSERT INTO student VALUES (2,'Ann',20,2/2/1997);
+INSERT INTO student VALUES (3,'hosea',30,2/2/1997);
+INSERT INTO student VALUES (4,'risper',10,2/2/1997);
+INSERT INTO student VALUES (5,'jude',60,2/2/1997);
+
+INSERT INTO kids VALUES (1,'EMMA',10,2/3/2000);
+INSERT INTO kids VALUES (2,'joan' ,9,2/3/2000);
+INSERT INTO kids VALUES (3,'Dennis',12,2/3/2000);
+INSERT INTO kids VALUES (4,'albert',11,2/3/2000);
+INSERT INTO kids VALUES (5,'wesley',15,2/3/2000);
+INSERT INTO kids VALUES (6,'chris',16,2/3/2000);
+
